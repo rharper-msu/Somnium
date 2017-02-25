b5797c0f7132e629500bf4119f66fbd8cca026d9
Richard Harper <rharper10@msudenver.edu>
2017-02-25 06:25:22 -0700

    Add a few calls to plaid module

--- somnium-web/app.js
+++ somnium-web/app.js
@@ -1,12 +1,23 @@
 var express = require('express');
+var plaid = require('plaid');
+
 var app = express();
 
 var PORT = 3000;
 
-app.get('/',function(req, res){
-    res.send("Somnium");
+var PLAID_ENV = plaid.environments.tartan;
+
+app.get('/', function (req, res) {
+
+    // plaid.getCategories(PLAID_ENV, function (err, data) {
+    //     res.send(data);
+    // });
+
+    plaid.getCategory('13005005',PLAID_ENV, function (err, data) {
+        res.send(data);
+    });
 });
 
-app.listen(PORT,function(){
+app.listen(PORT, function () {
     console.log('listening on port ' + PORT);
 });
