597ac440f0149cd5a2b83577734a479699a77a2a
Richard Harper <rharper10@msudenver.edu>
2017-02-25 16:23:17 -0700

    update the public endpoint to get plaid institutions

--- somnium-web/app.js
+++ somnium-web/app.js
@@ -17,17 +17,17 @@ app.get('/public', function (req, res) {
     //     res.send(data);
     // });  
 
-    // plaid.getInstitutions(PLAID_ENV, function (err, data) {
-    //     res.send(data);
-    // });
-
-    plaid.searchAllInstitutions({ id: '5301a93ac140de84910000e0' }, PLAID_ENV, function (err, data) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(data);
-        }
+    plaid.getInstitutions(PLAID_ENV, function (err, data) {
+        res.send(data);
     });
+
+    // plaid.searchAllInstitutions({ id: '5301a93ac140de84910000e0' }, PLAID_ENV, function (err, data) {
+    //     if (err) {
+    //         res.send(err);
+    //     } else {
+    //         res.send(data);
+    //     }
+    // });
 });
 
 app.get('/private',function(req, res){
